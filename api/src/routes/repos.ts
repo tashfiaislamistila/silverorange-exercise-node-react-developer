bf1b08f5f81bd9caf85c81051895097ae3d3c5aa
Tashfia Islam <tashfia.cse@gmail.com>
2022-08-29 21:24:55 -0700

    comments added in backend api

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -12,19 +12,21 @@ repos.get('/', async (_: Request, res: Response) => {
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   // res.json([]);
 
-  // for local api
+  //------------------------ for local api ---------------------------//
+
   //   const allFilteredData = data.filter((filteredData) => {
   //     return filteredData.fork === false;
   //   });
   //   res.json(allFilteredData);
 
-  // for GIT api
+  //-------------------- for GIT api---------------------------------//
 
   const result: AxiosResponse = await axios.get(
     `https://api.github.com/users/silverorange/repos`
   );
   const data = result.data;
   const allFilteredData = data.filter((filteredData: { fork: boolean }) => {
+    // Filtered data where the fork is false
     return filteredData.fork === false;
   });
   return res.status(200).json(allFilteredData);
