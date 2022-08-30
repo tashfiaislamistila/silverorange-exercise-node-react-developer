e41c323625b0724f482c5258d1b37b2d7ef45bc7
Tashfia Islam <tashfia.cse@gmail.com>
2022-08-29 21:58:42 -0700

    data sorted and displayed by reverse chronological order

--- web/src/CustomHooks/useRepositories.ts
+++ web/src/CustomHooks/useRepositories.ts
@@ -5,7 +5,12 @@ export default function useRepositories() {
     fetch('http://localhost:4000/repos')
       .then((res) => res.json())
       .then((data) => {
-        setRepos(data);
+        const sortedData = data.sort((a: any, b: any) => {
+          return (
+            new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+          );
+        });
+        setRepos(sortedData);
       });
   }, []);
   return [repos];
