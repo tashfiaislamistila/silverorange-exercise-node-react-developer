01e30e60e72e61b1104b5739aefa4eabd8acf668
Tashfia Islam <tashfia.cse@gmail.com>
2022-08-29 21:16:58 -0700

    local repo data returned in api endpoint

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import data from '../../data/repos.json'; //for local api
 
 export const repos = Router();
 
@@ -8,5 +9,10 @@ repos.get('/', async (_: Request, res: Response) => {
   res.status(200);
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+  // res.json([]);
+  // for local api
+  const allFilteredData = data.filter((filteredData) => {
+    return filteredData.fork === false;
+  });
+  res.json(allFilteredData);
 });
