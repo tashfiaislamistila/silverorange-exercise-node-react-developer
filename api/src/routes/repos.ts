915b2c43735a14064ee0e627516c5b4745109c80
Tashfia Islam <tashfia.cse@gmail.com>
2022-08-29 21:21:59 -0700

    repo data filtered with fork as false

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,5 +1,6 @@
+import axios, { AxiosResponse } from 'axios';
 import { Router, Request, Response } from 'express';
-import data from '../../data/repos.json'; //for local api
+// import data from '../../data/repos.json'; //for local api
 
 export const repos = Router();
 
@@ -10,9 +11,21 @@ repos.get('/', async (_: Request, res: Response) => {
 
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
   // res.json([]);
+
   // for local api
-  const allFilteredData = data.filter((filteredData) => {
+  //   const allFilteredData = data.filter((filteredData) => {
+  //     return filteredData.fork === false;
+  //   });
+  //   res.json(allFilteredData);
+
+  // for GIT api
+
+  const result: AxiosResponse = await axios.get(
+    `https://api.github.com/users/silverorange/repos`
+  );
+  const data = result.data;
+  const allFilteredData = data.filter((filteredData: { fork: boolean }) => {
     return filteredData.fork === false;
   });
-  res.json(allFilteredData);
+  return res.status(200).json(allFilteredData);
 });
